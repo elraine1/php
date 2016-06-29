9137f2f516b296d70580c416e42de18ed359b61a
Yun SeokMin <elraine@naver.com>
2016-06-29 15:51:26 +0900

    LCS 완성.

--- html/excercise/LCS/lcs_functions.php
+++ html/excercise/LCS/lcs_functions.php
@@ -4,7 +4,9 @@
 
 		$str1_arr = str_split($str1);
 		$str2_arr = str_split($str2);
+		$lcs_matrix = array();
 
+		/*
 		printf("<table>");
 		printf("<tr>");
 		printf("<th>＼</th>");
@@ -20,19 +22,111 @@
 			for($j=0 ; $j < count($str1_arr); $j++){
 				printf("<td>");
 				if(strcmp($str2_arr[$i], $str1_arr[$j])==0){
-					echo "o";
+					echo "&nbsp o &nbsp ";
 				}else {
-					echo " ";
+					echo "&nbsp &nbsp";
 				}
 				printf("</td>");
 			}
 			printf("</tr>");
 		}
 		printf("</table>");
+*/
+		printf("<hr>");
+		printf("<h4>Diff Table</h4>");
+
+		printf("<table>");
+		printf("<tr>");
+		printf("<th>＼</th>");
+		for($i=0 ; $i < count($str1_arr); $i++){
+			printf("<th> %s </th>", $str1_arr[$i]);
+		}
+		printf("</tr>");
+
+		for($i=0 ; $i < count($str2_arr); $i++){			
+			printf("<tr>");
+			printf("<th> %s </th>", $str2_arr[$i]);
+			
+			for($j=0 ; $j < count($str1_arr); $j++){
+				printf("<td>");
+				
+				if(strcmp($str2_arr[$i], $str1_arr[$j])==0){
+					$lcs_matrix[$i][$j] = "v";
+				}else {
+					$lcs_matrix[$i][$j] = "0";
+				}
+				printf("<input type='text' size='1' value='%s'>", $lcs_matrix[$i][$j]);
+				printf("</td>");
+			}
+			printf("</tr>");
+		}
+		printf("</table>");
+		
+		return $lcs_matrix;
 	}
 
+	// lcs_matrix  초기화.
+	function init_lcs_matrix($diff_matrix){
+		
+		$lcs_matrix = $diff_matrix;
+		$end_row = count($diff_matrix)-1;
+		$end_col = count($diff_matrix[0])-1;
+		
+//		echo $end_row . " " . $end_col . "<br>";
+		
+		for($i = $end_row; $i >= 0; $i--){
+			if($diff_matrix[$i][$end_col] == "v"){
+				$lcs_matrix[$i][$end_col] = 1;
+			}else{
+				$lcs_matrix[$i][$end_col] = 0;
+			}
+		}
+		
+		for($i = $end_col; $i >= 0; $i--){
+			if($diff_matrix[$end_row][$i] == "v"){
+				$lcs_matrix[$end_row][$i] = 1;
+			}else{
+				$lcs_matrix[$end_row][$i] = 0;
+			}
+		}
+		
+		return $lcs_matrix;
+	}
+	
+	// submatrix 생성함수.
+	function make_submatrix($lcs_matrix, $i, $j){
+		
+		$end_row = count($lcs_matrix)-1;
+		
+		$submatrix = array();
+		
+		while($end_row >= $i){
+			$submatrix[] = array_slice($lcs_matrix[$i], $j);
+			$i++;
+		}
+		return $submatrix;
+	}
+			
+			
+	// matrix 내에서 가장 큰 값 찾기.
+	function get_max_in_matrix($submatrix){
+		
+		$max = 0;
+		$rows = count($submatrix);
+		$cols = count($submatrix[0]);
+		for($i=0; $i < $rows; $i++){
+			for($j=0; $j < $cols; $j++){
+				if($max < $submatrix[$i][$j]){
+					$max = $submatrix[$i][$j];
+				}
+			}
+		}
+		return $max;
+	}
+			
+	
+			
+			
 	
 	
-
-
 ?>
--- html/excercise/LCS/lcs_process.php
+++ html/excercise/LCS/lcs_process.php
@@ -35,24 +35,81 @@
 			echo "str1: " . $str1 . "<br>";
 			echo "str2: " . $str2 . "<br><br>";
 			
-			make_diff_table($str1, $str2);
+			$str_diff_matrix = make_diff_table($str1, $str2);
 			
 			echo "<hr>";
+			///////////////////////////////
 			
-			//$str_concat = $str1 . $str2;
-			
-			
-			
+			$lcs_matrix = init_lcs_matrix($str_diff_matrix);
 			
 			
-			echo "<hr>";
+			$end_row = count($lcs_matrix)-1;
+			$end_col = count($lcs_matrix[0])-1;
+		
+			
+			for($i = $end_row-1 ; $i >= 0 ; $i--){
+				
+				for($j = $end_col-1 ; $j >= 0; $j--){
+					if($lcs_matrix[$i][$j] == "v"){
+						
+						$submatrix = make_submatrix($lcs_matrix, $i+1, $j+1);	
+						$max_in_submatrix = get_max_in_matrix($submatrix);
+						$lcs_matrix[$i][$j] = $max_in_submatrix + 1;
+					}
+				}
+			}
+			
+				
+			$str1_arr = str_split($str1);
+			$str2_arr = str_split($str2);
+				
+			printf("<h4>Route Table</h4>");
+			printf("<table>");
+			printf("<tr>");
+			printf("<th>＼</th>");
+			for($i=0 ; $i < count($str1_arr); $i++){
+				printf("<th> %s </th>", $str1_arr[$i]);
+			}
+			printf("</tr>");
+
+			for($i=0 ; $i < count($str2_arr); $i++){			
+				printf("<tr>");
+				printf("<th> %s </th>", $str2_arr[$i]);
+				
+				for($j=0 ; $j < count($str1_arr); $j++){
+					printf("<td>");
+					printf("<input type='text' size='1' value='%s'>", $lcs_matrix[$i][$j]);
+					printf("</td>");
+				}
+				printf("</tr>");
+			}
+			printf("</table>");
+			printf("<hr>");
+			
+			
+			$lcs = array();
+			$find = get_max_in_matrix($lcs_matrix);
+			$start_j = 0;	
+			for($i=0; $i <= $end_row; $i++){
+
+				for($j = $start_j ; $j <= $end_col; $j++){
+					if($lcs_matrix[$i][$j] === $find){
+						$lcs[] = $str1_arr[$j];			
+						$start_j = $j+1;		// column 탐색 시작점 변경. 
+						$find--;	
+						break; 
+					}
+				}
+				if($find==0){ break; }	// $find가 0이면 더이상 찾을 값이 없으므로 종료.
+			}
+			
+			echo "<br>LCS : " . join(" ", $lcs) . "<br><br>";
+			
 		?>	
 	
-		<hr>
 		<a href="./lcs_index.php"><button>다시하기</button></a><br>
 	</div>
 </body>
 
-  
-
+ 
 </html>
