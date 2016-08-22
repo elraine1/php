271aa83eb8876009278abcb47ee595686ca3a80a
Yun SeokMin <elraine@naver.com>
2016-08-22 16:52:45 +0900

    타이머 JS 덧붙임(다듬을 것).

--- html/excercise/mancala/process.php
+++ html/excercise/mancala/process.php
@@ -1,3 +1,8 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+
 <style type="text/css">
 	#mancala_board {
 		border: 2px solid gray;
@@ -31,9 +36,106 @@
 	input[type="button"]:disabled {
 		background: darkgray;
 	}
+	
+	
+	@keyframes example {
+		from {color: red;}
+		to {color: black;}
+	}
+
+	.mytimer {
+		animation-name: example; 
+		animation-duration: 1.5s; 	
+	}
+
+	.timer {
+		display: block; 
+		width: 800px; 
+		margin:0 auto; 
+		margin-top: 100px; 
+		font-size:100px	;
+		text-align: center;
+	}
+
+	.temp{
+		font-size:150px;
+	}
+
 </style>
 
 
+<script type="text/javascript" src="./jquery/jquery.js"></script>
+<script type="text/javascript" src="./jquery/jquery-ui.js"></script>
+<script type="text/javascript" src="./jquery/jquery.countdown.js"></script>
+<script>
+
+	$(document).ready(function(){
+		/*
+		window.onbeforeunload = function() {
+			return "게임을 마치지 않고 나가면 징계를 먹습니다.";
+		};
+		
+		window.onunload = function() {
+			// 만약 게임중이면 징계먹이기
+			return;
+		};
+		
+		// disable F5 key
+		function disableF5(e) { 
+			if ((e.which || e.keyCode) == 116) {
+				e.preventDefault(); 
+			}
+		}
+		$(document).on("keydown", disableF5);
+		*/
+		function flick() {			
+			/*
+			var els = document.getElementsByClassName('mytimer');
+			var flash = els[0];		
+			var newNode = flash.cloneNode(true);
+			newNode.id = flash.id + '1';
+			flash.parentNode.replaceChild(newNode, flash);
+			newNode.className = 'timer mytimer';
+			*/
+			$('#flash').addClass('temp', 900, callBack());
+			/*
+			$('#flash').animate({font-size:200px;
+									 color:red;    }, 1000);
+									 */
+			//flash.style.color = 'red';
+			//setTimeout(function() { flash.style.color = 'black'; }, 100);	
+		}
+		
+		function callBack(element){
+			$('#flash').removeClass('temp');
+		}
+		
+
+		$('#timer').countdown(Date.now() + 10000, function(event) { 
+			var remainingSecondsString =  event.strftime('%-S');
+			$(this).text(remainingSecondsString); 
+			if (parseInt(remainingSecondsString) == 0) {
+				$(this).text('Time Over');
+			} else if (parseInt(remainingSecondsString) % 2 == 0) {
+				$(this).css('color', 'red');
+			} else {
+				$(this).css('color', 'blue');
+			}
+		});
+		
+		$('#dummy').countdown(Date.now() + 10000, function(event) { 
+			flick();
+		});
+	});
+</script>
+
+</head>
+<body>
+
+
+<span id="timer" class="timer">Timer not started yet</span>
+<!-- <span id="flash" class="timer mytimer">Flash</span> -->
+<span id="dummy"></span>
 
 <h2> - Mancala! - </h2>
 <?php
@@ -149,15 +251,12 @@
 	</tr>
 	</table>
 	
-	
-	
-	
-	
-	
 	<br><br>
 	<a href="test.php"><button>go test.php</button></a>
-	
-	
+
+</div>
+</body>
+</html>
 	
 	
 	
